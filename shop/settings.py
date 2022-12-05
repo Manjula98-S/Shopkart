ed7481c1a70243cf420d164c91a8bff30b5d4257
Manjula98-S <manjuma120398@gmail.com>
2022-12-06 00:28:15 +0530

    first commit

--- shop/settings.py
+++ shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-8vj874+f2xfwe0n&2fhbrov#@*-k*zx4b&yhk3n9fjpos%7n&m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedMainfestStaticStorage"
 MEDIA_URL = '/images/'
 MEDIA_ROOT=BASE_DIR/'static'
 STATICFILES_DIRS=[

76e8015b254a749e77805d5771108db7a8df6703
Manjula98-S <manjuma120398@gmail.com>
2022-12-05 12:03:25 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn shopkart.wsgi 
---
+++ shopkart/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from shopkart.models import Category
+from shopkart.models import Product
+
+
+admin.site.register(Category)
+admin.site.register(Product)
